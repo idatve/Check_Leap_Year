dcee5f3c6d3528b798c85691ff29f067c3cde584
idatve <idatve1@gmail.com>
2021-11-04 19:24:11 +0100

    First commit removed a comment

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Lager et objekt av Check_Leap_Year
+
         Check_Leap_Year cly = new Check_Leap_Year();
 
         System.out.println(cly.isLeapYear(2000));
